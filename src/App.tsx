73090bdfc71dd98aeafa9a66cfcf113c50696256
yaroslavbez <47645087+yaroslavbez@users.noreply.github.com>
2026-03-04 17:05:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/risk-fluent-v2">
+      <BrowserRouter basename="/risk-guardian-studio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
